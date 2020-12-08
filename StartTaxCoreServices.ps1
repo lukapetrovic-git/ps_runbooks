4f3f3f011522ecca51547598111f3b5c2ddd20cc
Luka Petrovic <67110853+lukapetrovic-git@users.noreply.github.com>
2020-12-08 15:18:04 +0100

    Update StartTaxCoreServices.ps1

--- StartTaxCoreServices.ps1
+++ StartTaxCoreServices.ps1
@@ -1,3 +1,3 @@
-Get-Service -Name *TaxCore* -Computer FRCVMSPRODAPP11 |
+Get-Service -Name *TaxCore* -Computer Test |
 ? { $_.Status -eq "Stopped" -and $_.StartType -eq "Auto" } | Start-Service 
-Get-Service -Name *TaxCore* -Computer FRCVMSPRODAPP11 | format-table -AutoSize MachineName, ServiceName, DisplayName, Status, StartType
+Get-Service -Name *TaxCore* -Computer Test | format-table -AutoSize MachineName, ServiceName, DisplayName, Status, StartType
