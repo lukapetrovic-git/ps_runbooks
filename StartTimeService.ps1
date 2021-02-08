db83ceb4608c4751a75bde275432cb021f31b30a
Luka Petrovic <67110853+lukapetrovic-git@users.noreply.github.com>
2021-02-08 14:38:07 +0100

    Update StartTimeService.ps1

--- StartTimeService.ps1
+++ StartTimeService.ps1
@@ -1,3 +1,3 @@
-Get-Service -Name w32time -Computer Test |
+Get-Service -Name w32time -Computer _computerName_ |
 ? { $_.Status -eq "Stopped" } | Start-Service 
-Get-Service -Name w32time -Computer Test | format-table -AutoSize MachineName, ServiceName, DisplayName, Status, StartType 
+Get-Service -Name w32time -Computer _computerName_ | format-table -AutoSize MachineName, ServiceName, DisplayName, Status, StartType 
