4ddfdb021d73c784c128179a7d0e72b98fc1fb77
Luka Petrovic <67110853+lukapetrovic-git@users.noreply.github.com>
2021-02-08 14:38:16 +0100

    Update StartWebService.ps1

--- StartWebService.ps1
+++ StartWebService.ps1
@@ -1,3 +1,3 @@
-Get-Service -Name "W3SVC" -Computer Test |
+Get-Service -Name "W3SVC" -Computer _computerName_ |
 ? { $_.Status -eq "Stopped" -and $_.StartType -eq "Auto" } | Start-Service 
-Get-Service -Name "W3SVC" -Computer Test | format-table -AutoSize MachineName, ServiceName, DisplayName, Status, StartType 
+Get-Service -Name "W3SVC" -Computer _computerName_ | format-table -AutoSize MachineName, ServiceName, DisplayName, Status, StartType 
